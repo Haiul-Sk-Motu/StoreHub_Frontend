c57a75171b55999c89887a4dc34c8fc1bb191913
Haiul Sk <haiulsk23.04@gmail.com>
2026-08-28 23:05:58 +0530

    StoreHub Frontend

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    <>
+     <h1 className='text-blue-500'>Tailwind Test</h1>
+
+     <Button>Click Me</Button>
+    </>
+  )
+}
+
+export default App
